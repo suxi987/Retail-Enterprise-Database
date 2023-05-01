41c06fda0ab2773054d06e40b93caef2bdaaecde
suxi987 <108771890+suxi987@users.noreply.github.com>
2023-04-30 20:44:46 -0400

    Update DBS211_Business_reports.sql

--- DBS211_Business_reports.sql
+++ DBS211_Business_reports.sql
@@ -12,10 +12,8 @@ CREATE OR REPLACE VIEW monthend_region_gross_profit AS
     INNER JOIN warehouse w ON o.order_number = w.order_number
     INNER JOIN product p ON w.product_id = p.product_id
     
-    GROUP BY r.regionid, r.region_desc, r.country, to_date('2022-01-31', 'yyyy-mm-dd')
-    
+    GROUP BY r.regionid, r.region_desc, r.country, to_date('2022-01-31', 'yyyy-mm-dd')    
     ORDER BY gross_profit DESC;
-
 
 
 /* Management view: This report reflects monthly gross profit and ROS for each store that has generated sales in descending order,
@@ -29,9 +27,7 @@ CREATE OR REPLACE VIEW monthend_store_performance AS
     INNER JOIN store s ON o.store_number = s.store_number
     INNER JOIN region r ON s.regionid = r.regionid
     
-    GROUP BY o.store_number, s.regionid, s.address, r.region_desc, r.country, 
-to_date('2022-01-31', 'yyyy-mm-dd')
-    
+    GROUP BY o.store_number, s.regionid, s.address, r.region_desc, r.country, to_date('2022-01-31', 'yyyy-mm-dd')    
     ORDER BY total_sales DESC;
     
     
@@ -47,21 +43,16 @@ CREATE OR REPLACE VIEW monthend_employee_performence AS
     INNER JOIN product p ON w.product_id = p.product_id
     
     GROUP BY c.sales_rep, e.first_name ||' '|| e.last_name, e.role, e.store_number, e.reports_to, 
-    e.employment_date, to_date('2022-01-31', 'yyyy-mm-dd') 
-    
-    ORDER BY total_sales DESC;
-    
+    e.employment_date, to_date('2022-01-31', 'yyyy-mm-dd')     
+    ORDER BY total_sales DESC;   
     
     
 /* Store manager view: This report reflects the monthly best selling items across all stores and is able to show the trend of customer purchasing inclination, thus helping 
    the store manager on restocking decision*/
 CREATE OR REPLACE VIEW monthend_most_sales_item AS
     SELECT p.product_id, p.product_desc, p.msrp, p.cost, SUM(w.amount_ordered) As  total_quantity_sold, 
-    ROUND((p.msrp - p.cost) / (p.msrp), 2) AS return_on_sales, 
-    to_date('2022-01-31', 'yyyy-mm-dd') AS monthend
+    ROUND((p.msrp - p.cost) / (p.msrp), 2) AS return_on_sales, to_date('2022-01-31', 'yyyy-mm-dd') AS monthend
     
-    FROM product p INNER JOIN warehouse w ON p.product_id = w.product_id
-    
-    GROUP BY p.product_id, p.product_desc, p.msrp, p.cost, to_date('2022-01-31', 'yyyy-mm-dd')
-    
+    FROM product p INNER JOIN warehouse w ON p.product_id = w.product_id    
+    GROUP BY p.product_id, p.product_desc, p.msrp, p.cost, to_date('2022-01-31', 'yyyy-mm-dd')    
     ORDER BY total_quantity_sold DESC;
